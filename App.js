9b6abac753782d31174de01c35504c36d6c54399
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-06-13 16:37:58 +0200

    Add header component

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <View style={styles.navigation}>
+          <Image style={styles.img} source={require('./assets/app/logo.png')} />
+          <Image
+            style={styles.img}
+            source={require('./assets/app/search.png')}
+          />
+          <Image
+            style={styles.img}
+            source={require('./assets/app/heart.png')}
+          />
+          <Image style={styles.img} source={require('./assets/app/cart.png')} />
+        </View>
+        <Image
+          style={styles.img}
+          source={require('./assets/app/account.png')}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#efeff0',
+    borderBottomWidth: 1,
+    borderColor: '#d2d3d5',
+    shadowColor: 'red',
+    padding: 12,
+  },
+  navigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '50%',
+  },
+  img: {
+    width: 28,
+    height: 28,
   },
 });
